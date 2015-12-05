f946d5988298889a225812709cd1109e73ef6c0f
Jansen Kantor <jkantor@ccs.neu.edu>
2015-12-04 23:01:28 -0500

    added basic game, board, and tile stuff

--- pieces.py
+++ pieces.py
@@ -70,20 +70,53 @@ class AIPlayer (TsuroPlayer):
 		super(TsuroPlayer, self).__init__(hand, position)
 
 
+pipTransitions = {0:5, 1:4, 2:7, 3:6, 4:1, 5:0, 6:3, 7:2}
+class TsuroGame:
+	def __init__(self, players):
+		self.players = players
+		self.board = TsuroBoard()
+		self.tiles = [TsuroTile(tileInfo) for tileInfo in tiledata]
+	#transform plays a card
+	#play actually plays
+
 class TsuroBoard:
 	def __init__(self):
-		self.board = [[None * 6] * 6]
-	def placePiece(self, piece, location):
-		self.board[location[0], location[1]] = piece
-	def getPiece(self, location):
+		self.board = [[None * 8] * 8]
+	
+	def placeTile(self, tile, location):
+		if self.isOutsidePosition(location):
+			return -1
+		self.board[location[0], location[1]] = tile
+		return 0
+
+	def getTile(self, location): 
 		return self.board[location[0], location[1]]
+	
 	def followPath(self, location, pip):
-		
+		currentLocation = location
+		currentPip = pip
+		while True:
+			nextTileLocation = (currentLocation[0] + adder[currentPip][0], currentLocation[1] + adder[currentPip][1])
+			nextTilePip = pipTransitions[currentPip]
+			nextTile = self.getTile(nextLocation)
+			if self.isOutsidePosition(nextLocation):
+				return -1
+			elif nextTile is None:
+				return (currentLocation, currentPip)
+			else:
+				currentLocation = nextTileLocation
+				currentPip = nextTile.followPath(nextTilePip)
 
+	def isOutsidePosition(self, location):
+		return location[0] % 7 == 0 or
+			   location[1] % 7 == 0
 
+	#adjacentCards
 
+class TsuroTile:
+	def __init__(self, tileinfo):
+		self.
 
-class TsuroPiece:
 
 
 
