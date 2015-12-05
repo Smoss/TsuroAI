205d15b1342cd214fb6d8425faa5727617eba99d
smoss <Smoss117@gmail.com>
2015-12-04 21:44:29 -0500

    Added AI player

--- pieces.py
+++ pieces.py
@@ -54,18 +54,20 @@ class TsuroGame:
 
 class TsuroPlayer:
 	"""Contains the player's hand and position on the board."""
-	def init(self, hand, position):
+	def __init__(self, hand, position):
 		self.position = position
 		self.hand = hand
 		self.private_hand = deck - self.hand
 	def lost(self):
 		adder = positions_adders[self.position[2]]
 		return (position[0] + adder[0]) % 7 == 0 or (position[1] + adder[1]) % 7 == 0
-	def play(self, )
+	def play(self, card):
+		pass
 
 
 class AIPlayer (TsuroPlayer):
 	def init(self, hand, position):
+		super(TsuroPlayer, self).__init__(hand, position)
 
 
 class TsuroBoard:
