8669e1b42485329ef8a9e2ddb98c1dabf126e181
smoss <Smoss117@gmail.com>
2015-12-12 18:09:27 -0500

    Logic should be mostly finished

--- pieces.py
+++ pieces.py
@@ -86,21 +86,34 @@ class AIPlayer (TsuroPlayer):
 	def play(self):
 		self.play(self, self.select_card())
 	def select_card(self):
-		return self.traverse(-(float("inf"), self.private_hand, 5)
-	def traverse(self, best, knowledge, runs):
+		return self.traverse(-(float("inf"), self.private_hand, self.hand, 5)
+	def symmetry(self, card):
+		sym = 0
+		for pair in card:
+			for x in range(2, 8, 2):
+				if ((pair[0] + x) % 8, (pair[1] + x) % 8) in card:
+					sym += 1
+		return sym
+	def traverse(self, best, knowledge, hand, runs):
 		if runs == 0:
 			return best
 		for card in self.hand:
 			points = 0
-			if self.game.transform(card).players[self.id].lost():
+			if not self in self.game.transform(card).active_players:
 				points = lost_score
+			elif len(self.game.transform(card).active_players) == 1:
+				return (lost_score * -.1, card)
 			else:
 				g_state = copy.deepcopy(game)
 				for state, n_knowledge in gen_States(g_state, self.private_hand):
-					if self.game.transform(card).players[self.id].lost():
+					if not self in state.active_players:
 						points = lost_score * .3
+					elif len(state.active_players) == 1:
+						points = lost_score * -.1
 					else:
-						self.traverse(best, n_knowledge, runs - 1)
+						points += (self.position[0] + self.position[1]) * 100 + self.symmetry(card) * 300 - len(state.active_players) * 300
+						for card in n_knowledge:
+							points += .3 * self.traverse(best, n_knowledge - card, hand.append(card), runs - 1)[0]
 			if points > best[0]:
 				best = (points, card)
 		return best
